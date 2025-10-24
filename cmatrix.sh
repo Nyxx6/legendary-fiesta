9a0213f92c642ddfb573d882ea7a02d68b0ee7a6
Nyx <92603198+Nyxx6@users.noreply.github.com>
2025-10-24 18:03:18 +0200

    Create cmatrix.sh

---
+++ cmatrix.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Name of the docker image
+IMAGE_NAME="cmatrixImage"
+
+# Check si l'image existe
+if [[ "$(docker images -q $IMAGE_NAME 2> /dev/null)" == "" ]]; then
+    echo "Building the image $IMAGE_NAME..."
+    docker build -t $IMAGE_NAME .
+else
+    echo "Image $IMAGE_NAME already exists. Running the container..."
+fi
+
+# Run the Docker container
+docker run --rm -it $IMAGE_NAME
